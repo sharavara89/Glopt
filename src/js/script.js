1c9419a1d3b5646421ee45749078281452ec3db1
Dmytro <sharavara.d@icloud.com>
2020-12-11 01:47:19 +0100

    slider in reviews added

---
+++ src/js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    $('.reviews__slider').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        variableWidth: true,
+		slidesToShow: 3,
+        infinite: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+	});
+});
